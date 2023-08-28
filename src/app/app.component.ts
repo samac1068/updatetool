859980e33c577a5b1361da8731e3321c416839dc
Sean <samac1068@gmail.com>
2023-08-28 14:43:55 -0400

    230828: - Included a function to retrieve a bearer token from the API and store it. - Added interceptor for all POST request that will include sending the new bearer token. - Reorganized the initialization process of the application, the validation of the user execute earlier in the process.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -84,68 +84,11 @@ export class AppComponent implements OnInit {
     this.getApplicationBuild();
 
     // Only continue when the three areas above have finished processing
-    // @ts-ignore
     this.appInit["id"] = setInterval(() => {
       if(this.appInit["server"] && this.appInit["system"] && this.appInit["build"])
         clearInterval(this.appInit["id"]);
-        this.getToken();
+        this.userAuthenticate();
     }, 300);
-  }
-
-  getToken() {
-    this.data.getBearerToken(this.store.user.username)
-      .subscribe((bearer:any) => {
-        this.store.setBearerToken(bearer.data);
-
-        // If we don't have authorization to even user the API, there is no sense going forward.
-        if(this.store.getBearerToken() == "") {
-          this.conlog.log("No Authorization Token Received. User is not Authorized Access.");
-          window.alert("No Authorization Token Received. User is not Authorized Access.");
-        } else {
-          //Make sure the API is available before we start attempting to load anything
-          this.conlog.log("authorizationTokenValid");
-          this.initializeApp();
-        }
-      });
-  }
-
-  initializeApp(){
-    this.conlog.log("initializeApp");
-    this.conlog.log("API path: " + this.data.getWSPath());
-
-    // Get and manage the user access token
-    this.conlog.log("urlToken: " + this.urlToken);
-    this.conlog.log("Development Mode: " + this.store.isDevMode());
-    this.conlog.log("Network: " + this.store.system['webservice']['network']);
-    this.conlog.log(this.store.system['webservice']['type'] + ' webservice - devmode is ' + this.store.isDevMode());
-
-    // Need to get the JWT before we continue
-    this.userAuthenticate();
-  }
-
-  userAuthenticate() {    // Used to create and store a JWT for communications during the established session
-
-    this.finalizeAppInit();
-  }
-
-  finalizeAppInit(){
-    if(this.store.isDevMode() || this.store.system['webservice']['network'] == 'sipr') {
-      // Adding clarify log comments, so we know the difference during testing.
-      if(this.store.isDevMode())
-        this.conlog.log("Executing in DevMode, attempting to generate local token");
-      else
-        this.conlog.log("Executing on a simulated SIPR Network, attempting to generate local token");
-
-      if (this.urlToken == "" || this.urlToken == undefined) {
-        this.urlToken = "";
-        this.data.getLocalToken("sean.mcgill")  // Generate a token at this point and introduce it into the application.  - This is used for development only
-          .subscribe((result:any) => {
-            this.urlToken = result['tokensid'];  // This is a newly created token that contains all the information needed for me to identify the user - Having a token is mandatory
-            this.continueInitialization();
-          });
-      }
-    } else
-      this.continueInitialization();  // Process the urlToken sent to the app component - User does not have access without it.
   }
 
   getSystemConfig() {
@@ -165,31 +108,44 @@ export class AppComponent implements OnInit {
 
     this.store.setSystemValue('servers', results.servers);
     this.store.setSystemValue('databases', results.databases.sort((a,b) => {
-        if(a.id < b.id) return -1;
-        if(a.id > b.id) return 1;
-        return 0;
-      }));
+      if(a.id < b.id) return -1;
+      if(a.id > b.id) return 1;
+      return 0;
+    }));
     this.appInit['server'] = true;
   }
 
   getApplicationBuild() {
+    /*TODO - Modify exisiting data subscriber with new signature as done before. */
+    // Using new subscriber signature change
     this.data.getAppUpdates()
-      .subscribe((results) => {
-      // Organize as single dim array instead of the multi dim array from JSON and Sort in descending order based on BuildDate column and finally sort in System Variable
-      this.store.setSystemValue('build', this.store.sortArr(results["whatsnew"], "BuildDate"));
-      this.conlog.log("Retrieved all update items listed in locally stored JSON file.");
-      this.appInit['build'] = true;
-    },
-      error => {
-        alert("getApplicationBuild: " + error.message);
+      .subscribe({
+        next: (results) => {
+          // Organize as single dim array instead of the multi dim array from JSON and Sort in descending order based on BuildDate column and finally sort in System Variable
+          this.store.setSystemValue('build', this.store.sortArr(results["whatsnew"], "BuildDate"));
+          this.conlog.log("Retrieved all update items listed in locally stored JSON file.");
+          this.appInit['build'] = true;
+        },
+        error: (error) => {
+          alert("getApplicationBuild: " + error.message);
+        }
       });
   }
 
-  continueInitialization(){
-    this.conlog.log("continueInitialization");
-    if(this.urlToken != undefined){
-      this.validateCapturedToken();
-    } else {
+  userAuthenticate() {    // Used to create and store a JWT for communications during the established session
+    this.conlog.log("userAuthenticate");
+    if(this.urlToken != undefined) {      // in production mode with url token from Orders
+      this.validateCapturedToken();       //this.getToken();
+    } else if (this.urlToken == undefined && this.store.isDevMode()) {  // in development mode without url token from anywhere
+      this.data.getLocalToken('sean.mcgill')
+        .subscribe({
+          next: (result: string) => {
+            // Take the new fake and temporary token and push in variable.  Will be used as if received from Orders.
+            this.urlToken = result["tokensid"];
+            this.validateCapturedToken();
+          }
+        });
+    } else if (this.urlToken == undefined && !this.store.isDevMode()) { // in production mode without url token from anywhere
       alert("No Application Token Found - Your access cannot be validated, therefore, you are not permitted to use this application. Application Aborted. Returning to previous application.");
     }
   }
@@ -198,77 +154,78 @@ export class AppComponent implements OnInit {
   validateCapturedToken() {
     this.conlog.log("validateCaptureToken");
     this.data.validateUserToken(this.urlToken)
-    .subscribe(result => {
-      this.conlog.log("value returned from token validation is " + (result[0] != null));
-      if(result[0] != null) {
-        this.conlog.log("assigned returned userinfo: " + result[0]["Username"] + " / " + result[0]["sKey"]);
-        this.store.setUserValue("token", this.urlToken);
-        this.store.setUserValue("username", result[0]["Username"]);
-        this.store.setUserValue("initalapp", result[0]["InitalApp"]);
-        this.store.setUserValue("tokencreatedate", result[0]["CreateDate"]);
-        this.store.setUserValue("skey", result[0]["sKey"]);
+      .subscribe( result => {
+          this.conlog.log("value returned from token validation is " + (result[0] != null));
+          if(result[0] != null) {
+            this.conlog.log("assigned returned userinfo: " + result[0]["Username"] + " / " + result[0]["sKey"]);
+            this.store.setUserValue("token", this.urlToken);
+            this.store.setUserValue("username", result[0]["Username"]);
+            this.store.setUserValue("initalapp", result[0]["InitalApp"]);
+            this.store.setUserValue("tokencreatedate", result[0]["CreateDate"]);
+            this.store.setUserValue("skey", result[0]["sKey"]);
 
-        //Signal that user has been validated - They should be able to use the tool at this point.
-        this.getUserInformation();
-      } else {
-          // Need to account for people hitting the refresh or back buttons - The entry key needs to be regenerated to access the application again.
-          alert("The access entry key is now invalid. It is not recommended to use the refresh page at anytime while using this application.  You must close this tab and open from DAMPS-Orders.");
-      }
-    },
-      error => {
-        alert("validateCaptureToken: " + error.message);
-      });
+            //Signal that user has been validated - They should be able to use the tool at this point.
+            this.getUserInformation();
+          } else {
+            // Need to account for people hitting the refresh or back buttons - The entry key needs to be regenerated to access the application again.
+            alert("The access entry key is now invalid. It is not recommended to use the refresh page at anytime while using this application.  You must close this tab and open from DAMPS-Orders.");
+          }
+        },
+        error => {
+          alert("validateCaptureToken: " + error.message);
+        });
   }
 
   getUserInformation() {
     this.data.getUserInfo()
       .subscribe((results) => {
-      if(results[0] != undefined) {
-        this.conlog.log("userinfo was successfully retrieved. [UID: " + results[0].UserID + "]");
-        let row: any = results[0];
-        if(results[0].UserID != -9) {
-          this.store.setUserValue("fname", this.store.checkForNull(row["FirstName"]));
-          this.store.setUserValue("lname", this.store.checkForNull(row["LastName"]));
-          this.store.setUserValue("appdata", this.store.checkForNull(row["AppData"]));
-          this.store.setUserValue("lastversion", this.store.checkForNull(row["UpdateVersion"]));
-          this.store.setUserValue("lastversiondt", this.store.checkForNull(row["UpdateDate"]));
-          this.store.setUserValue("lastlogin", this.store.checkForNull(row["LastLogIn"]));
-          this.store.setUserValue("curlogin", this.store.checkForNull(row["CurrentLogIn"]));
-          this.store.setUserValue("userid", this.store.checkForNull(row["UserID"]));
-          this.store.setUserValue("priv", this.store.checkForNull(row["Priv"]));
+          if(results[0] != undefined) {
+            this.conlog.log("userinfo was successfully retrieved. [UID: " + results[0].UserID + "]");
+            let row: any = results[0];
+            if(results[0].UserID != -9) {
+              this.store.setUserValue("fname", this.store.checkForNull(row["FirstName"]));
+              this.store.setUserValue("lname", this.store.checkForNull(row["LastName"]));
+              this.store.setUserValue("appdata", this.store.checkForNull(row["AppData"]));
+              this.store.setUserValue("lastversion", this.store.checkForNull(row["UpdateVersion"]));
+              this.store.setUserValue("lastversiondt", this.store.checkForNull(row["UpdateDate"]));
+              this.store.setUserValue("lastlogin", this.store.checkForNull(row["LastLogIn"]));
+              this.store.setUserValue("curlogin", this.store.checkForNull(row["CurrentLogIn"]));
+              this.store.setUserValue("userid", this.store.checkForNull(row["UserID"]));
+              this.store.setUserValue("priv", this.store.checkForNull(row["Priv"]));
 
-          this.store.setUserValue("servername", this.store.system['webservice']['type'].toUpperCase());
-          this.store.setUserValue("server", '{0}');
+              this.store.setUserValue("servername", this.store.system['webservice']['type'].toUpperCase());
+              this.store.setUserValue("server", '{0}');
 
-          // In many cases, the network information will be the old version, but instead of modifying it, we are now just ignoring everything but the preferred database
-          if(this.store.checkForNull(row["Network"]).length > 0) {
-            if (row["Network"].indexOf("|") > -1 && row["Network"].indexOf("#") > -1) {
-              let n: any = row["Network"].split("|");
-              if (n[1] == undefined) n[1] = n[0];
-              let p: any = n[1].split("#");
-              this.store.setUserValue("database", p[1]);
+              // In many cases, the network information will be the old version, but instead of modifying it, we are now just ignoring everything but the preferred database
+              if(this.store.checkForNull(row["Network"]).length > 0) {
+                if (row["Network"].indexOf("|") > -1 && row["Network"].indexOf("#") > -1) {
+                  let n: any = row["Network"].split("|");
+                  if (n[1] == undefined) n[1] = n[0];
+                  let p: any = n[1].split("#");
+                  this.store.setUserValue("database", p[1]);
+                } else {
+                  // Use the new network system information
+                  this.store.setUserValue("database", row["Network"]);
+                }
+              }
+
+              //Signal that user data has been loaded
+              this.comm.userInfoLoaded.emit(true);
             } else {
-              // Use the new network system information
-              this.store.setUserValue("database", row["Network"]);
+              this.store.setUserValue("userid", row["UserID"]);
+              this.store.setUserValue("fname", row["FirstName"]);
+              this.store.setUserValue("lname", row["LastName"]);
+              this.store.setUserValue("appdata", row["AppData"]);
+
+              this.comm.noToolUserInfoFound.emit();  //Signaling that only part or none of the data was found
             }
+          } else {
+            this.comm.noToolUserInfoFound.emit();
           }
-
-          //Signal that user data has been loaded
-          this.comm.userInfoLoaded.emit(true);
-        } else {
-          this.store.setUserValue("userid", row["UserID"]);
-          this.store.setUserValue("fname", row["FirstName"]);
-          this.store.setUserValue("lname", row["LastName"]);
-          this.store.setUserValue("appdata", row["AppData"]);
-
-          this.comm.noToolUserInfoFound.emit();  //Signaling that only part or none of the data was found
-        }
-      } else {
-        this.comm.noToolUserInfoFound.emit();
-      }
-    },
+        },
         error=> {
-        alert("getUserInformation: " + error.message);
+          alert("getUserInformation: " + error.message);
         });
   }
+
 }
